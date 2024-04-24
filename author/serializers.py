5f27882c445aa9fe4228a9657bbbc3cb5cca1f96
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-04-24 23:37:22 +0300

    Solution

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,36 @@
 from rest_framework import serializers
+from .models import Author
 
 
-class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+class AuthorSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=64, required=True)
+    last_name = serializers.CharField(max_length=64, required=True)
+    pseudonym = serializers.CharField(
+        max_length=64,
+        required=False,
+        allow_blank=True
+    )
+    age = serializers.IntegerField(required=True)
+    retired = serializers.BooleanField(required=True)
+
+    def create(self, validated_data: dict) -> Author:
+        return Author.objects.create(**validated_data)
+
+    def update(self, instance: Author, validated_data: dict) -> Author:
+        instance.first_name = validated_data.get(
+            "first_name",
+            instance.first_name
+        )
+        instance.last_name = validated_data.get(
+            "last_name",
+            instance.last_name
+        )
+        instance.pseudonym = validated_data.get(
+            "pseudonym",
+            instance.pseudonym
+        )
+        instance.age = validated_data.get("age", instance.age)
+        instance.retired = validated_data.get("retired", instance.retired)
+        instance.save()
+        return instance
--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,12 @@
-# Create your urls here
+from rest_framework import routers
+from django.urls import path, include
+from .views import AuthorViewSet
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register("authors", AuthorViewSet, basename="manage")
+
+urlpatterns = [
+    path("", include(router.urls), name="list")
+]
+
+app_name = "author"
